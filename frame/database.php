80ce8729a17be19dbeac03664dc9da6a55c2de83
Aquarion <nicholas@istic.net>
2013-02-03 15:25:48 +0000

    Some new assets

---
+++ frame/database.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of database
+ *
+ * @author Nicholas
+ */
+class database {
+    //put your code here
+}
+
+?>
