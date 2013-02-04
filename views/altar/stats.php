001f7cb1d4884136ec972120ebf27f0286bc59ad
Aquarion <nicholas@istic.net>
2013-02-04 00:16:40 +0000

    Cleaner Nation stats display

--- views/altar/stats.php
+++ views/altar/stats.php
@@ -1,55 +1,81 @@
 <div class="container-fluid">
   <div class="row-fluid">
-    <div class="span1">
-       
-    </div>
-    <div class="span10">
+    <div class="span4">
         <?PHP include("navigation.php"); ?>
+    </div>
+    <div class="span8">
+        <h1 class="pull-right">Kudos by Nation sacrificing</h1>
     </div>
   </div>
   <div class="row-fluid">
-    <div class="span2 ">
+    <div class="span12 ">
         <ul class="thumbnails">
-            <li>
-                <a href="" class="thumbnail centered active" >
-                    <img src="/assets/home/img/nations/rome.png" />
-                    <div class="caption">Rome</div >
+            <li class="thumbnail centered span2" >
+                <a href="/stats/carthage/" >
+                    <img src="/assets/home/img/nations/smaller/carthage.png" />
+                    <div class="caption">Carthage</div >
                 </a>
             </li>
-            <li>
-                <a href="" class="thumbnail centered" >
-                    <img src="/assets/home/img/nations/persia.png" />
-                    <div class="caption">Persia</div >
-                </a>
-            </li>
-            <li>
-                <a href="" class="thumbnail centered" >
-                    <img src="/assets/home/img/nations/hellasphoenicia.png" />
-                    <div class="caption">Hellas Phoenicia</div >
-                </a>
-            </li>
-            <li>
-                <a href="" class="thumbnail centered" >
-                    <img src="/assets/home/img/nations/greece.png" />
-                    <div class="caption">Greece</div >
-                </a>
-            </li>
-            <li>
-                <a href="" class="thumbnail centered" >
-                    <img src="/assets/home/img/nations/egypt.png" />
+            <li class="thumbnail centered span2">
+                <a href="/stats/egypt" >
+                    <img src="/assets/home/img/nations/smaller/egypt.png" />
                     <div class="caption">Egypt</div >
                 </a>
             </li>
-            <li>
-                <a href="" class="thumbnail centered" >
-                    <img src="/assets/home/img/nations/carthage.png" />
-                    <div class="caption">Carthage</div >
+            <li class="thumbnail centered span2">
+                <a href="/stats/greece" >
+                    <img src="/assets/home/img/nations/smaller/greece.png" />
+                    <div class="caption">Greece</div >
+                </a>
+            </li>
+            <li class="thumbnail centered span2">
+                <a href="/stats/hellasphoenicia" >
+                    <img src="/assets/home/img/nations/smaller/hellasphoenicia.png" />
+                    <div class="caption">Hellas Phoenicia</div >
+                </a>
+            </li>
+            <li class="thumbnail centered span2">
+                <a href="/stats/persia" >
+                    <img src="/assets/home/img/nations/smaller/persia.png" />
+                    <div class="caption">Persia</div >
+                </a>
+            </li>
+            <li class="thumbnail centered span2">
+                <a href="/stats/rome" >
+                    <img src="/assets/home/img/nations/smaller/rome.png" />
+                    <div class="caption">Rome</div >
                 </a>
             </li>
         </ul>
     </div>
-    <div class="span8">
-        <table class="table" id="kudostotal">
+  </div>
+
+<?PHP
+    $results = array(
+        'Carthage' => 0,
+        'Egypt' => 0,
+        'Greece' => 0,
+        'Hellas Phoenicia' => 0,
+        'Persia' => 0,
+        'Rome' => 0
+    );
+    foreach($totals as $line){
+       $results[$line->nation] = $line->totalize;
+    }
+    asort($results);
+?>
+  <div class="row-fluid">
+    <div class="span2 centered" style="font-size: xx-large"><?PHP echo $results['Carthage'] ?></div>
+    <div class="span2 centered" style="font-size: xx-large"><?PHP echo $results['Egypt'] ?></div>
+    <div class="span2 centered" style="font-size: xx-large"><?PHP echo $results['Greece'] ?></div>
+    <div class="span2 centered" style="font-size: xx-large"><?PHP echo $results['Hellas Phoenicia'] ?></div>
+    <div class="span2 centered" style="font-size: xx-large"><?PHP echo $results['Persia'] ?></div>
+    <div class="span2 centered" style="font-size: xx-large"><?PHP echo $results['Rome'] ?></div>
+  </div>
+    
+  </div>
+    <div class="span8" id="chartLocation">
+        <table class="table hidden" id="kudostotal">
 	<caption>Kudos Total</caption>
 	<thead>
             <tr>
@@ -70,7 +96,7 @@
 </div>
 <script type="text/javascript">
 $(document).ready(function(){
-    $('#kudostotal').visualize({type: 'pie'});
+    $('#kudostotal').visualize({type: 'pie', width: "600px", height: "600px"}).appendTo("#chartLocation");
     
 });
 
