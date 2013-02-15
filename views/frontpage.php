a5074c2cd49fea36c4c61679fd9a496a282a739f
Aquarion <nicholas@istic.net>
2013-02-15 23:46:02 +0000

    Cleanup styles on front page, remove Kudos as a seperate entity

--- views/frontpage.php
+++ views/frontpage.php
@@ -3,34 +3,30 @@
       <!-- Three columns of text below the carousel -->
       <div class="row">
         <div class="span4 centered">
-          <img class="img-circle" data-src="holder.js/140x140">
-          <h2>Sacrifices</h2>
-          <p><img src="/assets/home/img/section-icons/Pawn-Yellow-icon.png" width="128"/></p>
+          <h2>Sacrifices &amp; Kudos</h2>
+          <p><a href="/altar"><img src="/assets/home/img/section-icons/Pawn-Yellow-icon.png" width="128"/></a></p>
+<!--          <p><img src="/assets/home/img/section-icons/Ying-Yang-icon.png" width="128"/></p>-->
           <p><a class="btn" href="/altar">View Sacrifices</a></p>
           <p><a class="btn" href="/altar/add">Add New</a></p>
         </div><!-- /.span4 -->
         <div class="span4 centered notyet">
-          <img class="img-circle" data-src="holder.js/140x140">
           <h2>Blessings</h2>
           <p><img src="/assets/home/img/section-icons/Gift-5-icon.png" width="128"/></p><p><a class="btn" href="#">View details &raquo;</a></p>
         </div><!-- /.span4 -->
         <div class="span4 centered notyet">
-          <img class="img-circle" data-src="holder.js/140x140">
-          <h2>Kudos</h2>
-          <p><img src="/assets/home/img/section-icons/Ying-Yang-icon.png" width="128"/></p>
+          <h2>Players</h2>
+          <p><img src="/assets/home/img/section-icons/App-Community-Help-icon.png" width="128"/></p>
           <p><a class="btn" href="#">View details &raquo;</a></p>
         </div><!-- /.span4 -->
     </div> <!-- /row -->
       <div class="row">
         <div class="span4 centered notyet">
-          <img class="img-circle" data-src="holder.js/140x140">
           <h2>Lesser Mysteries</h2>
           <p><img src="/assets/home/img/section-icons/Art-of-Chemistry-icon.png" width="128"/></p>
           <p><a class="btn" href="#">View details &raquo;</a></p>
         </div><!-- /.span4 -->
         <div class="span4 centered notyet">
-          <img class="img-circle" data-src="holder.js/140x140">
-          <h2>Greater Mysteries & Artifacts</h2>
+          <h2>Greater Mysteries &amp; Artifacts</h2>
           <p><img src="/assets/home/img/section-icons/nuclear-icon.png" width="128"/></p>
           <p><a class="btn" href="#">View details &raquo;</a></p>
         </div><!-- /.span4 -->
@@ -44,9 +40,6 @@
     </div> <!-- /row -->
       <div class="row">
         <div class="span4 centered notyet">
-          <h2>Players</h2>
-          <p><img src="/assets/home/img/section-icons/App-Community-Help-icon.png" width="128"/></p>
-          <p><a class="btn" href="#">View details &raquo;</a></p>
         </div><!-- /.span4 -->
     </div> <!-- /row -->
     </div> <!-- /container -->
