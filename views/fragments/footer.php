aedd29810c4860d99acd1a8f359b4398fa78204d
Aquarion <aquarion@profounddecisions.co.uk>
2016-06-07 16:11:23 +0100

    Prettiness upgrade

--- views/fragments/footer.php
+++ views/fragments/footer.php
@@ -1,7 +1,9 @@
 
 <hr/>
-<center>
-[ <a href="/news">New Stuff</a> | Written by <a href="http://www.aquarionics.com">Aquarion</a> | Design by Bootstrap, Icons by PD &amp; <a href="https://icons8.com/">Icons8</a> ]
-</center>
+<div class="footer">
+	<p class="words">
+		[ <a href="/news">New Stuff</a> | Written by <a href="http://www.aquarionics.com">Aquarion</a> | Design by Bootstrap, Icons by PD &amp; <a href="https://icons8.com/">Icons8</a> ]
+	</p>
+</div>
   </body>
 </html>
